ee9f9f4f3df25da4b6b3ec501adb308658821e9c
yuki <yuki.for.neutral@gmail.com>
2024-11-08 13:33:51 +0900

    Update image configuration to use remotePatterns for external images

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,14 @@
 // Next.jsでは、外部の画像を使用する場合、そのホスト名を明示的に設定する必要がある
 const nextConfig = {
   images: {
-    domains: ["fakestoreapi.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+        port: "",
+        search: "",
+      },
+    ],
   },
 };
 
