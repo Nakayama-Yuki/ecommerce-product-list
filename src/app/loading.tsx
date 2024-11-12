4b1cdcb526a3d08a917d5a3ffbc94478de61e8da
yuki <yuki.for.neutral@gmail.com>
2024-11-12 13:32:03 +0900

    improve skelton UI

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,13 +1,25 @@
-//スケルトンローディングのコンポーネント
-export default function SkeletonProductCard() {
+// スケルトンカードを表すコンポーネント
+function SkeletonCard() {
   return (
-    <div className="border p-4 rounded-lg shadow-lg flex flex-col items-center">
-      <div className="w-full h-48 bg-gray-300 mb-4 rounded animate-pulse"></div>
-      <div className="w-3/4 h-6 bg-gray-300 mb-2 rounded animate-pulse"></div>
-      <div className="w-full h-4 bg-gray-300 mb-2 rounded animate-pulse"></div>
-      <div className="w-5/6 h-4 bg-gray-300 mb-4 rounded animate-pulse"></div>
-      <div className="w-1/2 h-6 bg-gray-300 rounded animate-pulse"></div>
-      <div className="mt-4 w-full h-10 bg-gray-300 rounded animate-pulse"></div>
+    <div className="border p-4 rounded-lg shadow-lg animate-pulse">
+      <div className="bg-gray-300 h-48 w-full mb-4"></div>
+      <div className="h-6 bg-gray-300 rounded w-3/4 mb-2"></div>
+      <div className="h-4 bg-gray-300 rounded mb-4"></div>
+      <div className="h-6 bg-gray-300 rounded w-1/2 mb-4"></div>
+      <div className="mt-4 w-full bg-gray-300 h-10 rounded"></div>
+    </div>
+  );
+}
+
+// スケルトンローディングをグリッド表示するコンポーネント
+export default function SkeletonProductCard() {
+  const skeletons = Array.from({ length: 3 });
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+      {skeletons.map((_, index) => (
+        <SkeletonCard key={index} />
+      ))}
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 //  * usestaやuseEffectを使用していないので、useclientは今のところ不要
 //  todo
-//  商品のソート機能を追加する
-//  カート機能を追加する
+//playwrightのテストを追加する？
 
 import { Products } from "@/types/products"; // 追加
 import ProductCard from "@/components/ProductCard"; // 追加
+import { Suspense } from "react";
+import SkeletonProductCard from "@/app/loading";
 
 // RSCを使って関数コンポーネントを作成
 export default async function Home() {
@@ -20,11 +21,13 @@ export default async function Home() {
   return (
     <div className="container mx-auto py-10">
       <h1 className="text-4xl font-bold text-center mb-8">製品一覧</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+      <Suspense fallback={<SkeletonProductCard />}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </Suspense>
     </div>
   );
 }
