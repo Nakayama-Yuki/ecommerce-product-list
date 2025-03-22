3ef972910a94e386ae8304011b58f4e8b0a41a4e
yuki <yuki.for.neutral@gmail.com>
2025-03-22 11:43:58 +0900

    Add rating property to Products interface for enhanced product details

--- src/types/products.ts
+++ src/types/products.ts
@@ -3,6 +3,10 @@ export interface Products {
   title: string;
   price: number;
   description: string;
-  image: string;
   category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
 }
